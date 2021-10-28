4a8626a916a932c9bf6e72758eebc921ed17835d
Panu Kalliokoski <pkalliok@aiven.io>
2021-10-28 10:51:36 +0300

    Schema for services

--- schema.py
+++ schema.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 
 import strawberry
-from datetime import datetime
+from datetime import datetime, time
 from enum import Enum
 from typing import Optional
 from uuid import UUID
@@ -79,9 +79,189 @@ class PaymentCard:
 
 @strawberry.type
 class Service:
-    name: str
+    cloud: Cloud
+    # connection_info: Union[PgConnection,...] # including service_uri_params
+    create_time: datetime
+    features: list[FeatureFlag] # including termination_protection
+    maintenance: MaintenanceInfo
+    metadata: list[MetadataItem]
+    node_states: list[Node]
+    plan: str # should be enum
+    project_vpc: str
+    integrations: list[ServiceIntegration]
+    notifications: list[UserNotification]
+    resources: ServiceResources
+    service_parameters: ServiceParameters
+    service_type: ServiceType # service_type, service_type_description
+    service_uri: str
+    state: ServiceState # should be enum
+    update_time: datetime
+    # user_config: Union[PgUserConfig,...]
+    name: str # service_name
     project: Project
-    # TODO rest of fields
+
+@strawberry.enum
+class ServiceState(Enum):
+    POWEROFF = "POWEROFF"
+    RUNNING = "RUNNING"
+    REBUILDING = "REBUILDING"
+    REBALANCING = "REBALANCING"
+
+@strawberry.type
+class Backup:
+    backup_name: str
+    backup_time: datetime
+    data_size: int
+
+@strawberry.type
+class Cloud:
+    slug: str # cloud_name, should be enum
+    description: str
+
+@strawberry.type
+class FeatureFlag:
+    name: str
+    enabled: bool
+
+@strawberry.type
+class MaintenanceInfo:
+    day_of_week: DayOfWeek # dow
+    time_of_day: time # time
+    updates: list[MaintenanceUpdate]
+
+@strawberry.enum
+class DayOfWeek(Enum):
+    MONDAY = "monday"
+    TUESDAY = "tuesday"
+    WEDNESDAY = "wednesday"
+    THURSDAY = "thursday"
+    FRIDAY = "friday"
+    SATURDAY = "saturday"
+    SUNDAY = "sunday"
+    NEVER = "never"
+
+@strawberry.type
+class MaintenanceUpdate:
+    deadline: Optional[datetime]
+    description: str
+    start_after: Optional[datetime]
+    start_at: Optional[datetime]
+
+@strawberry.type
+class MetadataItem:
+    name: str
+    value: str
+
+@strawberry.type
+class Node:
+    name: str
+    # progress_updates: list[str]
+    role: NodeRole
+    state: NodeState
+
+@strawberry.enum
+class NodeRole(Enum):
+    MASTER = "master"
+    STANDBY = "standby"
+    READ_REPLICA = "read-replica"
+
+@strawberry.enum
+class NodeState(Enum):
+    SETTING_UP_VM = "setting_up_vm"
+    SYNCING_DATA = "syncing_data"
+    RUNNING = "running"
+    LEAVING = "leaving"
+    UNKNOWN = "unknown"
+
+@strawberry.type
+class ServiceComponent:
+    name: str
+    host: str
+    path: Optional[str]
+    port: int
+    route: RouteType
+    ssl: Optional[bool]
+    usage: UsageRole
+
+@strawberry.enum
+class RouteType(Enum):
+    DYNAMIC = "dynamic"
+    PUBLIC = "public"
+    PRIVATE = "private"
+    PRIVATELINK = "privatelink"
+
+@strawberry.enum
+class UsageRole(Enum):
+    PRIMARY = "primary"
+    REPLICA = "replica"
+
+@strawberry.type
+class ServiceIntegration:
+    id: UUID
+    active: bool
+    description: str
+    destination: ServiceIntegrationTarget
+    enabled: bool
+    # status: IntegrationStatus # TODO
+    integration_type: str # should be enum
+    source: ServiceIntegrationTarget
+    # user_config: Union[...]
+
+@strawberry.type
+class ServiceIntegrationTarget:
+    endpoint: Optional[IntegrationEndpoint]
+    project: Optional[Project]
+    service: Optional[Service]
+
+@strawberry.type
+class IntegrationEndpoint:
+    name: str
+    id: UUID
+
+@strawberry.type
+class UserNotification:
+    notification_type: NotificationType
+    level: NotificationLevel
+    message: str
+    help_article_url: Optional[str] # end_of_life_help_article_url
+    end_of_life_time: Optional[datetime]
+
+@strawberry.enum
+class NotificationLevel(Enum):
+    NOTICE = "notice"
+    WARNING = "warning"
+
+@strawberry.enum
+class NotificationType(Enum):
+    END_OF_LIFE = "end_of_life"
+    POWERED_OFF_REMOVAL = "powered_off_removal"
+
+@strawberry.type
+class ServiceResources:
+    # acls: list[ServiceACLRule]
+    backups: list[Backup]
+    components: list[ServiceComponent]
+    # connection_pools: list[ConnectionPool]
+    databases: list[str]
+    users: list[ServiceUser]
+
+@strawberry.type
+class ServiceParameters:
+    disk_space_mb: int
+    node_count: int
+    node_cpu_count: int
+    node_memory_mb: int
+
+@strawberry.type
+class ServiceType:
+    name: str # should be enum
+    description: str
+
+@strawberry.type
+class ServiceUser:
+    password: str
+    user_type: str # type, should be enum
+    username: str
 
 @strawberry.type
 class Query:
